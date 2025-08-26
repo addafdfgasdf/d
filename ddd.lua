29c95e222af13bc316e5298dc847ad2b88d547ed
addafdfgasdf <121368992+addafdfgasdf@users.noreply.github.com>
2025-08-27 04:01:45 +1000

    Update ddd.lua

--- ddd.lua
+++ ddd.lua
@@ -509,8 +509,8 @@ end
 
 local isActive = false
 
-local priorityNames1 = { "Amethyst", "Buoyant", "Party", "Delicious", "Primo" }
-local priorityNames2 = { "Golden", "Ruby", "Emerald", "Diamond", "BULL" }
+local priorityNames1 = { "Amethyst", "Golden", "Ruby", "Emerald", "Diamond", "BULL" }
+local priorityNames2 = { "Buoyant", "Party", "Delicious", "Primo" }
 
 UserInputService.InputBegan:Connect(function(input, gameProcessed)
 	if gameProcessed then return end
